a768df5c70a019cc2ba0709dbae8fb46f03a6b63
Quinn Lonergan <qui99lo@gmail.com>
2022-01-26 11:45:54 -0500

    get reviews method

--- app/controllers/scraper.rb
+++ app/controllers/scraper.rb
@@ -1,5 +1,11 @@
 class Scraper
+
     def get_page
         Nokogiri::HTML(open("https://www.lendingtree.com/reviews/personal/first-midwest-bank/52903183"))
     end
+
+    def get_reviews
+        self.get_page.css
+    end
+
 end
