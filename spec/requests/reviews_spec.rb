a7d98447a637857c1adfb54c2c455ce4eaae41d2
Quinn Lonergan <qui99lo@gmail.com>
2022-01-28 09:52:07 -0500

    more testing

--- spec/requests/reviews_spec.rb
+++ spec/requests/reviews_spec.rb
@@ -10,6 +10,15 @@ RSpec.describe "review API", :type => :request do
   #   end
   # end
 
+  describe "get_reviews" do
+    it "returns a list of reviews from a valid lendingtree url" do
+      post "/get_reviews", params: {url: "https://www.lendingtree.com/reviews/mortgage/loansnap/39777117"}
+      json_response = JSON.parse(response.body)
+      expect(response.status).to eq 201
+      expect(json_response.count).to be > 400
+    end
+  end
+
   # describe "get_reviews" do
   #   it "returns an error for an invalid url" do
   #     post "/get_reviews", params: {url: "www.google.com"}
@@ -19,12 +28,12 @@ RSpec.describe "review API", :type => :request do
   #   end
   # end
   
-  describe "get_reviews" do
-    it "returns an error for an invalid lendingtree url" do
-      post "/get_reviews", params: {url: "https://www.lendingtree.com/reviews/"}
-      json_response = JSON.parse(response.body)
-      expect(json_response).to eq "error" => "Must be lender URL"
-      expect(response.status).to eq 422
-    end
-  end
+  # describe "get_reviews" do
+  #   it "returns an error for an invalid lendingtree url" do
+  #     post "/get_reviews", params: {url: "https://www.lendingtree.com/reviews/"}
+  #     json_response = JSON.parse(response.body)
+  #     expect(json_response).to eq "error" => "Must be lender URL"
+  #     expect(response.status).to eq 422
+  #   end
+  # end
 end
